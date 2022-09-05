f00951e33109e58e578ee4e3ef804aacbc186660
Flo <flograttarola@gmail.com>
2022-09-05 15:47:07 +0200

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -26,7 +26,7 @@ var siteConfig = {
     // See https://www.gbif.org/developer/occurrence (long page without enough anchors - search for 'Occurrence Download Predicates')
     // The format is however slightly different, in that is use camelCase for keys instead of CONSTANT_CASE.
     rootPredicate: {
-      type: 'or',
+      type: 'and',
       'predicates': [
         {
           key: 'publishingOrg',
@@ -50,7 +50,7 @@ var siteConfig = {
         }
       ]
     },
-    highlightedFilters: ['taxonKey', 'stateProvince', 'year', 'basisOfRecord', 'publishingOrg','occurrenceIssue'],  
+    highlightedFilters: ['taxonKey', 'stateProvince', 'year', 'basisOfRecord', 'publishingOrg','occurrenceIssue'],
     occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'] // what tabs should be shown
     // see https://hp-theme.gbif-staging.org/data-exploration-config for more options
   }

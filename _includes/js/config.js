5e38a5d348922fcea8f58bd9ed8c4685e6efb81d
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2025-11-07 11:52:37 +0100

    update to config v 3

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,121 +1,153 @@
-var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
-  primary: themeStyle.colors.primary
-}});
-
-var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
-  primary: themeStyle.colors.primary,
-  fontSize: '16px'
-}});
-
 var siteConfig = {
-  routes: {
-    occurrenceSearch: {
-      // The route you are currently using for occurrence search. The language prefix will be added automatically
-      // If you need special routes per language, then you have to add locale specific overwrites. The page language is available as a global variable called `pageLang`
-      route: '/datos/buscar'
+  "version": 3,
+  "pages": [
+    {
+      "id": "occurrenceSearch",
+      "path": "datos/buscar"
     },
-    literatureSearch: {
-      route: '/literature/buscar'
+    {
+      "id": "literatureSearch",
+      "path": "literature/buscar"
+    }
+  ],
+  "disableInlineTableFilterButtons": false,
+  "availableCatalogues": [
+    "OCCURRENCE",
+    "LITERATURE"
+  ],
+  "dataHeader": {
+    "enableApiPopup": false,
+    "enableInfoPopup": false
+  },
+  "theme": {
+    "primary": themeStyle.colors.primary,
+    "borderRadius": 3,
+    "stickyOffset": "0px"
+  },
+  "maps": {
+    "locale": "es",
+    "mapStyles": {
+      "defaultProjection": "MERCATOR",
+      "defaultMapStyle": "BRIGHT",
+      "options": {
+        "MERCATOR": [
+          "BRIGHT",
+          "NATURAL",
+          "DARK"
+        ]
+      }
     }
   },
-  maps: {
-    locale: 'es',
-    defaultProjection: 'MERCATOR',
-    defaultMapStyle: 'BRIGHT',
-    mapStyles: {
-      MERCATOR: ['BRIGHT', 'NATURAL', 'DARK'],
+  "languages": [
+    {
+      "code": "es",
+      "localeCode": "es",
+      "label": "Español",
+      "default": true,
+      "textDirection": "ltr",
+      "cmsLocale": "es",
+      "vocabularyLocale": "es-ES",
+      "iso3LetterCode": "spa",
+      "gbifOrgLocalePrefix": "/es",
+      "grSciCollLocalePrefix": "/es",
+      "mapTileLocale": "es"
     },
-  },
-  occurrence: {
-    mapSettings: {
-      lat: -32.96,
-      lng: -56.19,
-      zoom: 6.58
-    },
-    // You probably need help to configure the scope - so just ask
-    // for his demo site we only show Fungi (taxonKey=5). It use the predicate structure known from GBIF download API.
-    // See https://www.gbif.org/developer/occurrence (long page without enough anchors - search for 'Occurrence Download Predicates')
-    // The format is however slightly different, in that is use camelCase for keys instead of CONSTANT_CASE.
-    rootPredicate: {
-      type: 'or',
-      predicates: [
+    {
+      "code": "en",
+      "localeCode": "en",
+      "label": "English",
+      "default": false,
+      "textDirection": "ltr",
+      "iso3LetterCode": "eng",
+      "cmsLocale": "en-GB",
+      "vocabularyLocale": "en",
+      "gbifOrgLocalePrefix": "",
+      "mapTileLocale": "en"
+    }
+  ],
+  "messages": {},
+  "occurrenceSearch": {
+    "scope": {
+      "type": "or",
+      "predicates": [
         {
-          'key': 'publishingOrg',
-          'type': 'equals',
-          'value': '862f7ec3-3134-4dce-ab5a-03c81f54bd72'
+          "key": "publishingOrg",
+          "type": "equals",
+          "value": "862f7ec3-3134-4dce-ab5a-03c81f54bd72"
         },
         {
-          'type': 'and',
-          'predicates': [
+          "type": "and",
+          "predicates": [
             {
-              'key': 'publishingOrg',
-              'type': 'equals',
-              'value': '28eb1a3f-1c15-4a95-931a-4af90ecb574d'
+              "key": "publishingOrg",
+              "type": "equals",
+              "value": "28eb1a3f-1c15-4a95-931a-4af90ecb574d"
             },
             {
-              'key': 'country',
-              'type': 'equals',
-              'value': 'UY'
-            },
+              "key": "country",
+              "type": "equals",
+              "value": "UY"
+            }
           ]
         }
       ]
     },
-    highlightedFilters: ['taxonKey', 'stateProvince', 'year', 'basisOfRecord', 'publisherKey'],
-    occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'] // what tabs should be shown
-    // see https://hp-theme.gbif-staging.org/data-exploration-config for more options
+    "highlightedFilters": [
+      "taxonKey",
+      "stateProvince",
+      "year",
+      "basisOfRecord",
+      "publishingOrg"
+    ],
+    "tabs": [
+      "map",
+      "table",
+      "gallery",
+      "datasets",
+      "download"
+    ],
+    "mapSettings": {
+      "lat": -32.96,
+      "lng": -56.19,
+      "zoom": 6.58
+    }
   },
-  literature: {
-    rootFilter: {
-      predicate: {
-        type: 'or',
-        predicates: [
-
-          // http://127.0.0.1:4000/literature/buscar/?publishingOrg=862f7ec3-3134-4dce-ab5a-03c81f54bd72
-          // {
-          //   "type": "equals",
-     	    //   "key": "doi",
-    	    //   "value": "10.1038/s41598-020-79074-8"
-          // },
-          // {
-          //   "type": "equals",
-     	    //   "key": "doi",
-    	    //   "value": "10.3897/bdj.8.e56850"
-          // },
-          // {
-          //   "type": "equals",
-     	    //   "key": "doi",
-    	    //   "value": "10.3897/bdj.7.e36226"
-          // },
-          // {
-          //   "type": "equals",
-     	    //   "key": "doi",
-    	    //   "value": "10.3897/biss.4.58973"
-          // },
-          // {
-          //   "type": "equals",
-     	    //   "key": "doi",
-    	    //   "value": "10.26462/28.1.1"
-          // },
-          {
-           'type': 'in',
-           'key': 'publishingOrganizationKey',
-           'values': ['862f7ec3-3134-4dce-ab5a-03c81f54bd72']
-          },
-          // {
-          //   'type': 'in',
-          //   'key': 'countriesOfCoverage',
-          //   'values': ['UY']
-          // }
-        ]
-      }
+  "collectionSearch": {},
+  "institutionSearch": {},
+  "datasetSearch": {},
+  "publisherSearch": {},
+  "literatureSearch": {
+    "scope": {
+      "type": "or",
+      "predicates": [
+        {
+          "type": "in",
+          "key": "publishingOrganizationKey",
+          "values": [
+            "862f7ec3-3134-4dce-ab5a-03c81f54bd72"
+          ]
+        }
+      ]
     },
-    highlightedFilters: ['q', 'countriesOfResearcher', 'countriesOfCoverage', 'year']
+    "highlightedFilters": [
+      "q",
+      "countriesOfResearcher",
+      "countriesOfCoverage",
+      "year"
+    ]
   }
 };
 
 // example of a language specific route overwrite
-if (pageLang === 'en')  {
-  siteConfig.routes.occurrenceSearch.route = '/occurrence/search';
-  siteConfig.routes.literatureSearch.route = '/literature/search';
+if (pageLang === 'en') {
+  siteConfig.pages = [
+    {
+      "id": "occurrenceSearch",
+      "path": "occurrence/search"
+    },
+    {
+      "id": "literatureSearch",
+      "path": "literature/search"
+    }
+  ];
 }
